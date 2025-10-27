9b88253fc4328335325efa11131ea3b8e43580b1
AddisBelayhu <addisgithub@gmail.com>
2025-10-27 21:25:50 +0300

    today

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
-const pool = require("pg").pool
+const Pool = require("pg").Pool
 
 const pool = new Pool({
     user: "postgres",
--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
 const cors = require("cors")
-const pool = requir("./db")
+const pool = require("./db")
 
 //middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json()) //req.body
 
+//ROUTES//
+//Create a todo
+app.post("/todos", async(req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUE($1)", 
+            [description]);
+            res.json(newTodo);
+    } 
+    
+    catch (err) {
+        console.error(err.message)
+    } 
+})
 app.listen(5000, () => {
     console.log("Server is running on port 5000")
 })
